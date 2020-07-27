c1272a23e0844f2af1417cf4aba3dcb24dc3b94a
DavinciB <66717845+DavinciB@users.noreply.github.com>
2020-07-28 00:53:57 +0530

    Update quartiles.py

--- Day1/quartiles.py
+++ Day1/quartiles.py
@@ -6,14 +6,12 @@ Q2 = np.median(A)
 if N % 2 == 0 :
     A1 = A[:int((N/2))]
     A2 = A[int((N/2)):N]
-    Q1 = np.median(A1)
-    Q3 = np.median(A2)
 else :
     m = (N//2)
     A1 = A[:m]
     A2 = A[(m+1):N]
-    Q1 = np.median(A1)
-    Q3 = np.median(A2)
+Q1 = np.median(A1)
+Q3 = np.median(A2)
 print(int(Q1))
 print(int(Q2))
 print(int(Q3))
